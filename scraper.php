09be2c7f1c0b575512c4c4856dc20417a8723f62
Your Neighbourhood <yourneighbourhood@users.noreply.github.com>
2020-04-22 22:28:34 +1000

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,74 +1,94 @@
 <?php
-require_once 'vendor/autoload.php';
-require_once 'vendor/openaustralia/scraperwiki/scraperwiki.php';
-
-use PGuardiario\PGBrowser;
-use Torann\DomParser\HtmlDom;
-
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+// Townsville City Council Development Applications scraper
+// (ICON Software Solutions PlanningXchange)
+// Sourced from http://eplanning.townsville.qld.gov.au/Pages/XC.Track/SearchApplication.aspx?ss=sq
+// Formatted for http://www.planningalerts.org.au/
 date_default_timezone_set('Australia/Sydney');
-
-# Default to 'thisweek', use MORPH_PERIOD to change to 'thismonth' or 'lastmonth' for data recovery
-switch(getenv('MORPH_PERIOD')) {
-    case 'thismonth' :
-        $period = 'thismonth';
-        break;
-    case 'lastmonth' :
-        $period = 'lastmonth';
-        break;
-    default         :
-        $period = 'thisweek';
-        break;
-}
-print "Getting data for `" .$period. "`, changable via MORPH_PERIOD environment\n";
-
+$date_format = 'Y-m-d';
+$cookie_file = '/tmp/cookies.txt';
 $comment_url = 'mailto:eplanning@townsville.qld.gov.au';
-$url_base = 'http://eplanning.townsville.qld.gov.au/Pages/XC.Track/SearchApplication.aspx';
-$url_query = 'k=LodgementDate&t=PDMCUCode,PDMCUimp,PDOpWorks,PDReconfig,QMCU,QRAL,QOPW,QDBW,QPOS,QEXC,QSPS,QCAR,PDSAMCUse,PDSARecon,PDSAOpWks';
-
-$browser = new PGBrowser();
-$full_url = $url_base. '?' .$url_query. '&d=' .$period;
-$page = $browser->get($full_url);
-
-$page_dom = HtmlDom::fromString($page->html);
-$results = $page_dom->find("div[class=result]");
-
-foreach ($results as $result) {
-    $info_url = explode("?", $result->find('a',0)->href);
-    $info_url = $url_base . '?' . $info_url[1];
-
-    // getting detail page
-    $page2 = $browser->get($info_url);
-    $page2_dom = HtmlDom::fromString($page2->html);
-    $divs = $page2_dom->find("div[class=detailleft]");
-
-    foreach ($divs as $div) {
-        switch ($div->plaintext) {
-            case 'Description:' :
-                $description = trim($div->nextSibling()->plaintext);
-                break;
-            case 'Properties:' :
-                $address = explode("\n", $div->nextSibling()->plaintext);
-                $address = trim(preg_replace('/\s+/', ' ', $address[0]));
-                break;
-            case 'Lodged:' :
-                $date_received = explode("/", $div->nextSibling()->plaintext);
-                $date_received = $date_received[2] . '-' . $date_received[1] . '-' . $date_received[0];
-                break;
-        }
+$terms_url = 'http://eplanning.townsville.qld.gov.au/Common/Common/Terms.aspx';
+$rss_feed = 'http://eplanning.townsville.qld.gov.au/Pages/XC.Track/SearchApplication.aspx?o=rss&d=last14days&t=PDMCUCode,PDMCUimp,PDOpWorks,PDReconfig';
+$url_baselink = 'http://eplanning.townsville.qld.gov.au/Pages/XC.Track/SearchApplication.aspx';
+print "Scraping eplanning.townsville.qld.gov.au...\n";
+//accept_terms($terms_url, $cookie_file);
+// Download and parse RSS feed (last 14 days of applications)
+$curl = curl_init($rss_feed);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; PlanningAlerts/0.1; +http://www.planningalerts.org.au/)");
+$rss_response = curl_exec($curl);
+curl_close($curl);
+$rss_response = preg_replace('/utf-16/i', 'utf-8', $rss_response);
+$rss = simplexml_load_string($rss_response);
+// Iterate through each application
+foreach ($rss->channel->item as $item)
+{
+    // RSS title appears to be the council reference
+    $rss_title = explode('-', $item->title);
+    $council_reference = trim($rss_title[0]);
+    // RSS description appears to be the address followed by the actual description
+    $rss_description = preg_split('/\./', $item->description, 2);
+    // But of course some have no address, ugh...
+    if(count($rss_description) != 2) {
+        print "Description field missing an address for application $council_reference";
+        continue;
     }
-
-    $record = [
-        'council_reference' => $result->find('a',0)->plaintext,
+    $address = trim($rss_description[0]);
+    $description = trim($rss_description[1]);
+    $info_url = $url_baselink . trim((string)$item->link);
+    //Publication date is a full ISO format string. eg. 2015-06-02T16:12:34.34+10:00
+    //But we only want the date part.
+    $date_scraped = date($date_format);
+    $date_received = date($date_format, strtotime($item->pubDate));
+    $application = array(
+        'council_reference' => $council_reference,
         'address' => $address,
         'description' => $description,
         'info_url' => $info_url,
-        'comment_url' => $comment_url,
-        'date_scraped' => date('Y-m-d'),
+        'comment_url' => $comment_url . "?subject=" . urlencode("Development Application Enquiry: {$council_reference}"),
+        'date_scraped' => $date_scraped,
         'date_received' => $date_received
-    ];
-
-    # Check if record exist, if not, INSERT, else do nothing
-    print ("Saving record " .$record['council_reference']. " - " .$record['address']. "\n");
-//         print_r ($record);
-    scraperwiki::save(['council_reference'], $record);
+    );
+    //Check to see if the record has already been inserted into the database.
+    if (scraperwiki::get_var($council_reference) == "")
+    {
+      //No record found. Insert.
+      print "New application found. Inserting ".$council_reference."\n";
+      scraperwiki::save_sqlite(array('council_reference'), $application);
+      scraperwiki::save_var($council_reference, $council_reference);
+    }
+    else
+    {
+      //Record is found, so skip.
+      print "Record found. Skipping ".$council_reference."\n";
+    }
 }
+function accept_terms($terms_url, $cookie_file)
+{
+    $curl = curl_init($terms_url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie_file);
+    curl_setopt($curl, CURLOPT_COOKIEFILE, $cookie_file);
+    $terms_response = curl_exec($curl);
+    curl_close($curl);
+    preg_match('/<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="(.*)" \/>/', $terms_response, $viewstate_matches);
+    $viewstate = $viewstate_matches[1];
+    preg_match('/<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="(.*)" \/>/', $terms_response, $eventvalidation_matches);
+    $eventvalidation = $eventvalidation_matches[1];
+    $postfields = array();
+    $postfields['__VIEWSTATE'] = $viewstate;
+    $postfields['__EVENTVALIDATION'] = $eventvalidation;
+    $postfields['ctl00$ctMain1$BtnAgree'] = 'I Agree';
+    $postfields['ctl00$ctMain1$chkAgree$ctl02'] = 'on';
+    $curl = curl_init($terms_url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $postfields);
+    curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie_file);
+    curl_setopt($curl, CURLOPT_COOKIEFILE, $cookie_file);
+    curl_exec($curl);
+    curl_close($curl);
+}
+?>
